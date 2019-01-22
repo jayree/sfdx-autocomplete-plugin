2f7860d718e7aef353e31294a0271bea754cb7e4
jayree <jayree@users.noreply.github.com>
2019-01-22 01:11:07 +0100

    chore: tests

--- test/commands/autocomplete/create.test.ts
+++ test/commands/autocomplete/create.test.ts
@@ -7,6 +7,8 @@ import Create from '../../../src/commands/autocomplete/create';
 const root = path.resolve(__dirname, '../../../package.json');
 const config = new Config({ root });
 
+const AC_PLUGIN_PATH = path.resolve(__dirname, '..', '..', '..', '..');
+
 // autocomplete will throw error on windows ci
 const skipwindows = process.platform === 'win32' ? describe.skip : describe;
 
@@ -33,22 +35,61 @@ skipwindows('autocompleteCreate', () => {
     expect(cmd.zshCompletionSettersPath).to.eq(`${dir}/autocomplete/commands_setters`);
   });
 
-  test.it('#bashSetupScript', () => {
-    expect(cmd.bashSetupScript).to.eq(
-      `SFDX_AC_BASH_COMPFUNC_PATH=${
-        global.config.cacheDir
-      }/autocomplete/functions/bash/sfdx.bash && test -f $SFDX_AC_BASH_COMPFUNC_PATH && source $SFDX_AC_BASH_COMPFUNC_PATH;\n`
-    );
+  it('#genCompletionDotsFunc', () => {
+    expect(cmd.completionDotsFunc).to.eq(`expand-or-complete-with-dots() {
+  echo -n "..."
+  zle expand-or-complete
+  zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots`);
   });
 
-  test.it('#zshSetupScript', () => {
-    expect(cmd.zshSetupScript).to.eq(`
+  it('#bashSetupScript', () => {
+    const shellSetup = cmd.bashSetupScript;
+    expect(shellSetup).to.eq(`SFDX_AC_ANALYTICS_DIR=${global.config.cacheDir}/autocomplete/completion_analytics;
+SFDX_AC_COMMANDS_PATH=${global.config.cacheDir}/autocomplete/commands;
+SFDX_AC_BASH_COMPFUNC_PATH=${AC_PLUGIN_PATH}/autocomplete/bash/sfdx.bash && test -f $SFDX_AC_BASH_COMPFUNC_PATH && source $SFDX_AC_BASH_COMPFUNC_PATH;
+`);
+  });
+
+  it('#zshSetupScript', () => {
+    const shellSetup = cmd.zshSetupScript;
+    expect(shellSetup).to.eq(`expand-or-complete-with-dots() {
+  echo -n "..."
+  zle expand-or-complete
+  zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
+SFDX_AC_ANALYTICS_DIR=${global.config.cacheDir}/autocomplete/completion_analytics;
+SFDX_AC_COMMANDS_PATH=${global.config.cacheDir}/autocomplete/commands;
+SFDX_AC_ZSH_SETTERS_PATH=\${SFDX_AC_COMMANDS_PATH}_setters && test -f $SFDX_AC_ZSH_SETTERS_PATH && source $SFDX_AC_ZSH_SETTERS_PATH;
 fpath=(
-${global.config.cacheDir}/autocomplete/functions/zsh
+${AC_PLUGIN_PATH}/autocomplete/zsh
 $fpath
 );
 autoload -Uz compinit;
 compinit;
 `);
+  });
+
+  it('#zshSetupScript (w/o ellipsis)', () => {
+    const oldEnv = process.env;
+    process.env.SFDX_AC_ZSH_SKIP_ELLIPSIS = '1';
+    const shellSetup = cmd.zshSetupScript;
+
+    expect(shellSetup).to.eq(`
+SFDX_AC_ANALYTICS_DIR=${global.config.cacheDir}/autocomplete/completion_analytics;
+SFDX_AC_COMMANDS_PATH=${global.config.cacheDir}/autocomplete/commands;
+SFDX_AC_ZSH_SETTERS_PATH=\${SFDX_AC_COMMANDS_PATH}_setters && test -f $SFDX_AC_ZSH_SETTERS_PATH && source $SFDX_AC_ZSH_SETTERS_PATH;
+fpath=(
+${AC_PLUGIN_PATH}/autocomplete/zsh
+$fpath
+);
+autoload -Uz compinit;
+compinit;
+`);
+    process.env = oldEnv;
   });
 });
