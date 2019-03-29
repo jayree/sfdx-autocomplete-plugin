b9906782fe2b69c569d5247f3e326fee5f7af0b6
jayree <jayree@users.noreply.github.com>
2019-03-29 11:30:21 +0100

    chore: update lib

--- lib/commands/autocomplete/script.d.ts
+++ lib/commands/autocomplete/script.d.ts
@@ -1,12 +1,12 @@
 import { AutocompleteBase } from '../../base';
 export default class Script extends AutocompleteBase {
-  static description: string;
-  static hidden: boolean;
-  static args: {
-    name: string;
-    description: string;
-    required: boolean;
-  }[];
-  run(): Promise<void>;
-  private readonly prefix;
+    static description: string;
+    static hidden: boolean;
+    static args: {
+        name: string;
+        description: string;
+        required: boolean;
+    }[];
+    run(): Promise<void>;
+    private readonly prefix;
 }
