5a325f59079e5a3877f33c9ddcc8794f74c40026
jayree <jayree@users.noreply.github.com>
2019-03-29 11:21:33 +0100

    chore: add lib folder to support git install

---
+++ lib/commands/autocomplete/doctor.d.ts
@@ -0,0 +1,16 @@
+import { flags } from '@salesforce/command';
+import { AutocompleteBase } from '../../base';
+export default class Doctor extends AutocompleteBase {
+  static hidden: boolean;
+  static description: string;
+  static args: {
+    name: string;
+    description: string;
+    required: boolean;
+  }[];
+  protected static flagsConfig: {
+    debug: flags.Discriminated<flags.Boolean<boolean>>;
+  };
+  run(): Promise<void>;
+  private printList;
+}
