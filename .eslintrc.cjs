818b9a1881fae052d2bd2cd3c2789a758b08cc7b
jayree <jayree@users.noreply.github.com>
2025-11-16 12:54:52 +0100

    chore: fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,27 @@
 /*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * Copyright 2025, Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // Generated - Do not modify. Controlled by @salesforce/dev-scripts
 // See more at https://github.com/forcedotcom/sfdx-dev-packages/tree/master/packages/dev-scripts
 
 module.exports = {
-  extends: ['eslint-config-salesforce-typescript', 'plugin:sf-plugin/migration', 'plugin:prettier/recommended'],
-  plugins: ['eslint-plugin-header'],
-  ignorePatterns: ['**/*.d.ts', '**/*.cjs'],
-  rules: {
-    'header/header': [
-      2,
-      'block',
-      [
-        '',
-        {
-          pattern: ' \\* Copyright \\(c\\) \\d{4}, jayree',
-          template: ' * Copyright (c) 2023, jayree',
-        },
-        ' * All rights reserved.',
-        ' * Licensed under the BSD 3-Clause license.',
-        ' * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
-        ' ',
-      ],
-    ],
-  },
+  extends: [
+    'eslint-config-salesforce-typescript',
+    'eslint-config-salesforce-license',
+    'plugin:sf-plugin/recommended',
+    'plugin:prettier/recommended',
+  ],
 };
